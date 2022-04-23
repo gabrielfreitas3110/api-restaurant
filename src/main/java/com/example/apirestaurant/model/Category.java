433b3d232ea89f721f7d20a8377d310c7e621832
Gabriel.freitas <gabriel.freitas3110@gmail.com>
2022-04-23 00:53:32 -0300

    add jpa and h2 maven dependecies

--- src/main/java/com/example/apirestaurant/model/Category.java
+++ src/main/java/com/example/apirestaurant/model/Category.java
@@ -5,12 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "TB_CATEGORY")
 public class Category {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
     private String name;
 }
