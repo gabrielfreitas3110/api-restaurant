079e55514123179642cd6531e32677d0afc7173e
Gabriel.freitas <gabriel.freitas3110@gmail.com>
2022-04-23 00:05:43 -0300

    First commit - project created

---
+++ src/test/java/com/example/apirestaurant/ApiRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
